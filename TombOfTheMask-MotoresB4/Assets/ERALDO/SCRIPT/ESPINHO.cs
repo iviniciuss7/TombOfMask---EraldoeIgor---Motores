fcd642da72d16d057921825f1d2ac8e3132d8531
Eraldo Soares da Silva Filho <eraldo.soares@escolar.ifrn.edu.br>
2023-12-29 17:59:35 -0300

    commit

---
+++ TombOfTheMask-MotoresB4/Assets/ERALDO/SCRIPT/ESPINHO.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ESPINHO : MonoBehaviour
+{
+    [SerializeField]Player player;
+    [SerializeField] LayerMask mask;
+    [SerializeField] Rigidbody2D rig;
+    RaycastHit2D hit;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("")
+        {
+
+        }
+    }
+}
