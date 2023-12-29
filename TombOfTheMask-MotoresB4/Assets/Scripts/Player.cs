9438ec5566c7b3294bf6dbe0747beddf6cd9fe7e
iviniciuss7 <oliveira.igor@escolar.ifrn.edu.br>
2023-12-29 16:36:52 -0300

    MovimentoPlayer

--- TombOfTheMask-MotoresB4/Assets/Scripts/Player.cs
+++ TombOfTheMask-MotoresB4/Assets/Scripts/Player.cs
@@ -3,19 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Player : MonoBehaviour
 {
+
+    enum Direcoes
+    {
+       Cima, Baixo, Direita, Esquerda 
+    }
     [Header("Atributos")]
     public float forcaDoMov;
     public int health;
+    private Direcoes movimento;
 
     [Header("Componentes")] 
     private Animator anim;
     private Rigidbody2D rig;
 
     [Header("Booleanos")] 
-    public bool isMove;
+    private bool isMove;
+    private bool podeMover;
     
     
     void Start()
@@ -28,48 +36,110 @@ public class Player : MonoBehaviour
     void Update()
     {
         MovePlayer();
-        
+    }
+
+    private void FixedUpdate()
+    {
+        switch (movimento)
+        {
+            case Direcoes.Cima:
+                rig.velocity = new Vector2(0f, forcaDoMov);
+                break;
+            
+            case Direcoes.Baixo:
+                rig.velocity = new Vector2(0f, -forcaDoMov);
+                break;
+            case Direcoes.Direita:
+                rig.velocity = new Vector2(forcaDoMov, 0f);
+                break;
+            case Direcoes.Esquerda:
+                rig.velocity = new Vector2(-forcaDoMov, 0f);
+                break;
+        }
     }
 
     private void MovePlayer()
     {
-        
-    }
+        if (isMove)
+        {
+            if(Physics2D.Raycast(transform.position, Vector2.left))
+            {
+                podeMover = true;
+            }
 
-    IEnumerator SlideMovePLayer()
-    {
-        if (!isMove){
-            if (Input.GetKey(KeyCode.D))
+            else
             {
-                rig.velocity = new Vector2(forcaDoMov, 0f);
-                isMove = true;
+                podeMover = false; 
             }
-            else if (Input.GetKey(KeyCode.A))
+        }
+
+        if (podeMover)
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0)
             {
-                rig.velocity = new Vector2(-forcaDoMov, 0f);
+                rig.constraints = RigidbodyConstraints2D.FreezePositionY;
                 isMove = true;
+            
+                if (Input.GetAxisRaw("Horizontal") > 0)
+                {
+                    movimento = Direcoes.Direita;
+                    podeMover = false;
+                }
+                else
+                {
+                    movimento = Direcoes.Esquerda;
+                    podeMover = false;
+                }
             }
         
-            else if (Input.GetKey(KeyCode.W))
+            if (Input.GetAxisRaw("Vertical") != 0)
             {
-                rig.velocity = new Vector2(0f, forcaDoMov);
+                rig.constraints = RigidbodyConstraints2D.FreezePositionX;
                 isMove = true;
-            }
-            else if (Input.GetKey(KeyCode.S))
-            {
-                rig.velocity = new Vector2(0f, -forcaDoMov);
-                isMove = true;
+            
+                if (Input.GetAxisRaw("Vertical") > 0)
+                {
+                    movimento = Direcoes.Cima;
+                }
+                else
+                {
+                    movimento = Direcoes.Baixo;
+                }
             }
         }
     }
+    
     
     
     private void OnCollisionEnter2D(Collision2D colPlayer)
     {
         if (colPlayer.gameObject.CompareTag("Wall"))
         {
-            isMove = false;
+            
+        }
+        
+        if (colPlayer.gameObject.CompareTag("Enemy")) 
+        {
+            
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D colPlayer)
+    {
+        if (colPlayer.gameObject.CompareTag("Indicador"))
+        {
+            
+        }
+        
+        if (colPlayer.gameObject.CompareTag("Coin"))
+        {
+            
+        }
+        
+        if (colPlayer.gameObject.CompareTag("Star"))
+        {
+            
         }
     }
-    
 }
