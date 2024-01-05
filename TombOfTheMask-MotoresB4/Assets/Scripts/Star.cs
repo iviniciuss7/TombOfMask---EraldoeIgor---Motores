87c5c5229ffccd407b8fa1b9a4e63ea46455bf71
Speedzyy <eraldo.soares@escolar.ifrn.edu.br>
2024-01-05 11:44:59 -0300

    Contador de estrela

--- TombOfTheMask-MotoresB4/Assets/Scripts/Star.cs
+++ TombOfTheMask-MotoresB4/Assets/Scripts/Star.cs
@@ -1,19 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Star : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public int scoreValue;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
+            GameManager.instance.UpdateScore(scoreValue);
             Destroy(gameObject);
         }
     }
